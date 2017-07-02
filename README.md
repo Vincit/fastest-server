2c4c7cea9b92e2d7338c1ba5890e634c51b77927
Sami Koskimäki <sami.koskimaki@vincit.com>
2017-07-02 19:23:05 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Server for the Fastest mobile test framework
+
+```js
+const { Server } = require('fastest-server');
+const { AndroidTester } = require('fastest-tester');
+
+describe('My app tests', () => {
+  const port = 12345;
+
+  let server;
+  let tester;
+
+  before(() => {
+    server = new Server({
+      port,
+      // Android SDK path. If omitted ANDROID_HOME env variable is used.
+      sdkPath: '/path/to/sdk/root'
+    });
+
+    return server.start();
+  });
+
+  before(() => {
+    tester = new AndroidTester({
+      serverUrl: `http://localhost:${port}`,
+
+      app: 'path/to/the/app.apk',
+      packageName: 'fi.your.package.name'
+      platformName: 'Android',
+      platformVersion: '6.0',
+
+      // Name of the android virtual device (if using emulator).
+      avdName: 'Sony_xperia_z5_compact_API_23',
+      // The android emulator device name.
+      deviceName: 'emulator-5554'
+    });
+
+    return tester.init();
+  });
+
+  it('should test some crap', () => {
+    return tester
+      // Click a button with text 'Do stuff'.
+      .button('Do stuff').click()
+      // Wait until a text view with text 'Some stuff was done' becomes visible.
+      .textView('Some stuff was done').waitVisible()
+      // Guess what this does?
+      .flickRight()
+      // Test that something is visible without waiting.
+      .textView('I became visible').isVisible()
+      // Get all visible text inputs (EditTexts) and write some text to them.
+      .textInputs()
+        .at(0).setValue('foo')
+        .at(1).setValue('bar')
+        // Last visible text input.
+        .reverseAt(0).setValue('eggs')
+      // Select an element by android id and get its text.
+      .viewByAndroidId('some_id').text().then(text => {
+        console.log(text);
+      });
+  });
+
+});
+```
---
+++ tests/tests.js
@@ -0,0 +1,210 @@
+const expect = require('expect.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { Server } = require('../');
+const { Tester } = require('fastest-tester');
+
+describe('Fastest server', () => {
+  const port = 4723;
+  const localAppServerPort = 6100;
+  const sdkPath = '/foo/bar'
+  
+  let adbCalls = [];
+  let adbResults = [];
+  let spawnCalls = []; 
+
+  let server;
+  let appServer;
+
+  class MockAndroidCommandLineTools extends Server.AndroidCommandLineTools {
+    execAdb({cmd}) {
+      adbCalls.push(cmd);
+      console.log('execAdb:', cmd);
+      return Promise.resolve({
+        stdout: adbResults.shift(),
+        stderr: ''
+      });
+    }
+
+    spawn(...args) {
+      console.log('spawn:', ...args);
+      spawnCalls.push(args);
+    }
+  }
+
+  class MockServer extends Server {
+    static get AndroidCommandLineTools() {
+      return MockAndroidCommandLineTools;
+    }
+  }
+
+  before(() => {
+    server = new MockServer({
+      port,
+      localAppServerPort,
+      sdkPath
+    }); 
+
+    return server.start();   
+  });
+
+  before(done => {
+    appServer = express();
+    appServer.use(bodyParser.json());
+
+    appServer.requests = [];
+    appServer.responses = [];
+
+    appServer.use((req, res) => {
+      appServer.requests.push({
+        method: req.method,
+        path: req.path,
+        body: req.body
+      });
+
+      res.send(appServer.responses.shift());
+    });
+
+    appServer.server = appServer.listen(localAppServerPort, done);
+  });
+
+  after(done => {
+    appServer.server.close(done);
+  });
+
+  beforeEach(() => {
+    appServer.requests = [];
+    appServer.responses = [];
+    
+    adbCalls = [];
+    adbResults = [];
+    spawnCalls = []; 
+  });
+
+  describe('init', () => {
+
+    it('device not running', () => {
+      const tester = new Tester({
+        serverUrl: `http://localhost:${port}`,
+        deviceName: 'emulator-5554',
+        packageName: 'fi.foo.bar',
+        avdName: 'super-duper-avd',
+        app: '/path/to/app.apk'
+      });
+
+      // Response to the `ping` request that determines if the
+      // app server is running.
+      appServer.responses = [
+        {}
+      ];
+
+      adbResults = [
+        // devices
+        `List of devices attached
+        emulator-007 somecrap`,
+
+        // shell pm list packages
+        `not.our.package`,
+
+        // uninstall fi.foo.bar
+        ``,
+
+        // install /path/to/app.apk
+        ``,
+
+        // shell dumpsys package fi.foo.bar
+        `
+          requested permissions:
+          fi.some.permission
+          fi.some.other.permission
+          install permissions:
+        `,
+
+        // shell monkey -p fi.foo.bar -c android.intent.category.LAUNCHER 1
+        ``,
+
+        // forward tcp:6100 tcp:7100
+        ``,
+      ];
+
+      return tester.init().then(() => {
+        expect(adbCalls).to.eql([ 
+          'devices',
+          'shell pm list packages',
+          'uninstall fi.foo.bar',
+          'install /path/to/app.apk',
+          'shell dumpsys package fi.foo.bar',
+          'shell monkey -p fi.foo.bar -c android.intent.category.LAUNCHER 1',
+          'forward tcp:6100 tcp:7100'
+        ]);
+
+        expect(spawnCalls).to.have.length(1);
+        expect(spawnCalls[0]).to.eql([
+          "/foo/bar/emulator/emulator", [
+            '-avd',
+            'super-duper-avd'
+          ], {
+            stdio: 'ignore'
+          }
+        ]);
+      });
+    });
+
+    it('device already running', () => {
+      const tester = new Tester({
+        serverUrl: `http://localhost:${port}`,
+        deviceName: 'emulator-5554',
+        packageName: 'fi.foo.bar',
+        app: '/path/to/app.apk'
+      });
+
+      // Response to the `ping` request that determines if the
+      // app server is running.
+      appServer.responses = [
+        {}
+      ];
+
+      adbResults = [
+        // devices
+        `List of devices attached
+        emulator-5554 somecrap`,
+
+        // uninstall fi.foo.bar
+        ``,
+
+        // install /path/to/app.apk
+        ``,
+
+        // shell dumpsys package fi.foo.bar
+        `
+          requested permissions:
+          fi.some.permission
+          fi.some.other.permission
+          install permissions:
+        `,
+
+        // shell monkey -p fi.foo.bar -c android.intent.category.LAUNCHER 1
+        ``,
+
+        // forward tcp:6100 tcp:7100
+        ``,
+      ];
+
+      return tester.init().then(() => {
+        expect(adbCalls).to.eql([ 
+          'devices',
+          'uninstall fi.foo.bar',
+          'install /path/to/app.apk',
+          'shell dumpsys package fi.foo.bar',
+          'shell monkey -p fi.foo.bar -c android.intent.category.LAUNCHER 1',
+          'forward tcp:6100 tcp:7100'
+        ]);
+
+        expect(spawnCalls).to.have.length(0);
+      });
+    });
+
+  });
+
+});
